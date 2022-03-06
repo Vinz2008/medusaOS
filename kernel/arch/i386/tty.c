b009ff92f9aa34e8fdbfe5277ee66b2068042783
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-03-06 15:06:56 +0100

    started backslash n implementation in printf

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -15,6 +15,7 @@ static size_t terminal_row;
 static size_t terminal_column;
 static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
+int IsBacklash = false;
 
 void terminal_initialize(void) {
 	terminal_row = 0;
@@ -40,6 +41,19 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 
 void terminal_putchar(char c) {
 	unsigned char uc = c;
+	if (uc == "\\"[0]){
+	IsBacklash = true;
+	}
+	else if (IsBacklash == true) {
+		if (uc == "n"[0]) {
+			terminal_column = 0;
+			++terminal_row;
+		}
+		IsBacklash = false;
+	}
+	else {
+	IsBacklash = false;
+	}
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -57,5 +71,4 @@ void terminal_write(const char* data, size_t size) {
 
 void terminal_writestring(const char* data) {
 	terminal_write(data, strlen(data));
-	terminal_row = 0;
 }
