3f171c69a89154080830d653dc18f6cfdaed24b8
Vincent <vincentbidarddelanoe@gmail.com>
2022-07-21 19:10:54 +0200

    added memtest in grub

--- iso.sh
+++ iso.sh
@@ -10,4 +10,16 @@ mkdir -p isodir/modules
 cp -r sysroot/* isodir/
 #cp sysroot/boot/kernel.kernel isodir/boot/kernel.kernel
 cp grub/* isodir/boot/grub/
+
+if ![ -e x.memtest86plus ]
+then
+git clone https://github.com/memtest86plus/memtest86plus.git
+fi
+cd memtest86plus
+cd build32
+make
+cd ../..
+cp memtest86plus/build32/memtest.bin isodir/boot/memtest.bin
+
+
 grub-mkrescue /usr/lib/grub/i386-pc -o kernel.iso isodir
